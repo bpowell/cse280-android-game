d2788b67e833831d9900f44f504bdd4ed7fdce8a
Brandon Powell <bpowell29a@gmail.com>
2012-09-22 13:13:12 -0400

    Initial commit of Android code.

---
+++ game/gen/edu/oakland/cse280/bscd/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.oakland.cse280.bscd;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ game/src/edu/oakland/cse280/bscd/MainGameActivity.java
@@ -0,0 +1,15 @@
+package edu.oakland.cse280.bscd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainGameActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
