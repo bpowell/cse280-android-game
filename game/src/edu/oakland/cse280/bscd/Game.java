a6dceeb648dfd35a00eea5775917ca66153e8c09
Brandon Powell <bpowell29a@gmail.com>
2012-10-20 18:46:37 -0400

    Added game loop to Game.java. Organized imports.

--- game/src/edu/oakland/cse280/bscd/Game.java
+++ game/src/edu/oakland/cse280/bscd/Game.java
@@ -3,13 +3,14 @@ package edu.oakland.cse280.bscd;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class Game extends SurfaceView implements SurfaceHolder.Callback
 {
-	//private GameThread game_loop;
+	private GameThread game_loop;
 	//private Hero hero;
 	//private Map map;
 
@@ -29,11 +30,31 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback
 	@Override
 	public void surfaceCreated(SurfaceHolder holder)
 	{
+		if(game_loop == null || !game_loop.isAlive())
+		{
+			game_loop = new GameThread(getHolder(), this);
+			game_loop.setRunning(true);
+		}
+
+		game_loop.start();
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder)
 	{
+		while(true)
+		{
+			try
+			{
+				game_loop.setRunning(false);
+				game_loop.join();
+				return;
+			}
+			catch(Exception e)
+			{
+				Log.d("Game/surfaceDestroyed", "Cannot end game_loop thread.");
+			}
+		}
 	}
 
 	@Override
