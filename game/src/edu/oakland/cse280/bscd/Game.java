c4160eb525dcd938aae1cce56818210da6060f13
Brandon Powell <bpowell29a@gmail.com>
2012-10-20 18:40:28 -0400

    Main game class created.

---
+++ game/src/edu/oakland/cse280/bscd/Game.java
@@ -0,0 +1,49 @@
+package edu.oakland.cse280.bscd;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+public class Game extends SurfaceView implements SurfaceHolder.Callback
+{
+	//private GameThread game_loop;
+	//private Hero hero;
+	//private Map map;
+
+	private Context context;
+
+	public Game(Context context)
+	{
+		super(context);
+		this.context = context;
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
+	{
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder)
+	{
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder)
+	{
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		return true;
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas)
+	{
+	}
+}
