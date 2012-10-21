9bc6c0dc1be4dc83f00119fc8bbc9539a679ab08
Brandon Powell <bpowell29a@gmail.com>
2012-10-21 18:58:42 -0400

    Added a and b buttons to be displayed.

--- game/src/edu/oakland/cse280/bscd/graphics/UI/UIButtons.java
+++ game/src/edu/oakland/cse280/bscd/graphics/UI/UIButtons.java
@@ -45,7 +45,21 @@ public class UIButtons
 		Rect bounds = canvas.getClipBounds();
 
 		dpad_location.left = bounds.left+10;
-		dpad_location.top = bounds.bottom-60;
+		dpad_location.top = bounds.bottom-70;
+		dpad_location.right = dpad_location.left+60;
+		dpad_location.bottom = dpad_location.top+60;
 		canvas.drawBitmap(dpad, dpad_location.left, dpad_location.top, opake);
+
+		a_button_location.left = bounds.right-70;
+		a_button_location.top = bounds.bottom-150;
+		a_button_location.right = a_button_location.left+60;
+		a_button_location.bottom = a_button_location.top+60;
+		canvas.drawBitmap(a_button, a_button_location.left, a_button_location.top, opake);
+
+		b_button_location.left = bounds.right-70;
+		b_button_location.top = bounds.bottom-70;
+		b_button_location.right = b_button_location.left+60;
+		b_button_location.bottom = b_button_location.top+60;
+		canvas.drawBitmap(b_button, b_button_location.left, b_button_location.top, opake);
 	}
 }
