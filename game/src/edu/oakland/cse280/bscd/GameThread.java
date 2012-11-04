335c3d80d3755a7b4aad325bead3fa151e4fb768
Brandon Powell <bpowell29a@gmail.com>
2012-11-03 20:31:38 -0400

    Limited fps to 33.

--- game/src/edu/oakland/cse280/bscd/GameThread.java
+++ game/src/edu/oakland/cse280/bscd/GameThread.java
@@ -3,12 +3,15 @@ package edu.oakland.cse280.bscd;
 import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
+import java.lang.System;
+
 public class GameThread extends Thread
 {
 	private SurfaceHolder holder;
 	private Game game;
 
 	private boolean running;
+	private long start_time;
 
 	public boolean getRunning()
 	{
@@ -25,6 +28,8 @@ public class GameThread extends Thread
 		super();
 		this.holder = holder;
 		this.game = game;
+
+		start_time = System.currentTimeMillis();
 	}
 
 	@Override
@@ -35,11 +40,23 @@ public class GameThread extends Thread
 		{
 			canvas = null;
 			try{
+				long end_time = System.currentTimeMillis();
+				long dt = end_time - start_time;
+
+				if(dt < 33)
+					this.sleep(33 - dt);
+
+				start_time = System.currentTimeMillis();
+
 				canvas = this.holder.lockCanvas();
 				synchronized(this.holder)
 				{
 					this.game.onDraw(canvas);
 				}
+			}
+			catch(Exception e)
+			{
+				this.interrupt();
 			}
 			finally{
 				if(canvas != null)
