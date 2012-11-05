71314344e220ea0db55064cbb871cabbbbc3bb65
zabador <zabador@Minty.(none)>
2012-11-04 23:25:46 -0500

    able to launch game from load screen

--- game/gen/edu/oakland/cse280/bscd/R.java
+++ game/gen/edu/oakland/cse280/bscd/R.java
@@ -19,8 +19,14 @@ public final class R {
         public static final int tiles=0x7f020005;
         public static final int town=0x7f020006;
     }
+    public static final class id {
+        public static final int save1=0x7f050000;
+        public static final int save2=0x7f050001;
+        public static final int save3=0x7f050002;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int load_screen=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
---
+++ game/src/edu/oakland/cse280/bscd/LoadScreen.java
@@ -0,0 +1,58 @@
+package edu.oakland.cse280.bscd;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoadScreen extends Activity
+{
+    private TextView save1;
+    private TextView save2;
+    private TextView save3;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.load_screen);
+        save1 = (TextView)findViewById(R.id.save1);
+        save2 = (TextView)findViewById(R.id.save2);
+        save3 = (TextView)findViewById(R.id.save3);
+
+        save1.setText("Hero 1\r\nLevel 1\r\n");
+        save2.setText("New Game");
+        save3.setText("New Game");
+	}
+
+    public void save1(View view)
+    {
+        for(int i=0;i<10;i++)
+            Log.i("save1 ", "was Clicked");
+        Intent i = new Intent(LoadScreen.this, MainGameActivity.class);                      
+        startActivity(i);
+    }
+    
+    public void save2(View view)
+    {
+        for(int i=0;i<10;i++)
+            Log.i("save2 ", "was Clicked");
+        Intent i = new Intent(LoadScreen.this, MainGameActivity.class);                      
+        startActivity(i);
+    }
+
+    public void save3(View view)
+    {
+        for(int i=0;i<10;i++)
+            Log.i("save3 ", "was Clicked");
+        Intent i = new Intent(LoadScreen.this, MainGameActivity.class);                      
+        startActivity(i);
+    }
+
+
+}
