fd4ef24543f3ba712fc81daf99685e700be1383f
Brandon Powell <bpowell29a@gmail.com>
2012-11-10 15:53:14 -0500

    Added NPC base class.

---
+++ game/src/edu/oakland/cse280/bscd/graphics/entities/NPC.java
@@ -0,0 +1,9 @@
+package edu.oakland.cse280.bscd.graphics.entities;
+
+public class NPC extends Mobs
+{
+	public NPC(Bitmap sheet, int direction, Rect starting_position)
+	{
+		super(sheet, direction, starting_position);
+	}
+}
