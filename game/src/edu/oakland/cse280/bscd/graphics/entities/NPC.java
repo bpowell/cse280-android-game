475f1619004bd8595aa2c9d1586db57e129cef7f
Brandon Powell <bpowell29a@gmail.com>
2012-11-10 15:56:25 -0500

    Added missing imports to NPC

--- game/src/edu/oakland/cse280/bscd/graphics/entities/NPC.java
+++ game/src/edu/oakland/cse280/bscd/graphics/entities/NPC.java
@@ -1,5 +1,8 @@
 package edu.oakland.cse280.bscd.graphics.entities;
 
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+
 public class NPC extends Mobs
 {
 	public NPC(Bitmap sheet, int direction, Rect starting_position)
